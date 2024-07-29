23717d45ccf8f0b17a8c2997fe48c1741109e89f
leonardogonzalolaura <glleonardodaniel@gmail.com>
2024-07-28 19:23:33 -0500

    add progress and template

--- pipsetup/generator.py
+++ pipsetup/generator.py
@@ -1,4 +1,5 @@
 import os
+from pipsetup.progress.progress import ProgressIndicator
 
 class ProjectGenerator:
     def __init__(self, project_name: str, author: str, email: str):
@@ -8,29 +9,37 @@ class ProjectGenerator:
         self.templates_dir = os.path.join(os.path.dirname(__file__), 'templates')
     
     def create(self):
-        os.makedirs(self.project_name, exist_ok=True)
-        self._create_file('setup_py_template.py', 'setup.py')
-        self._create_file('setup_cfg_template.cfg', 'setup.cfg')
-        self._create_file('manifest_template.in', 'MANIFEST.in')
-        self._create_file('readme_template.md', 'README.md')
-        self._create_github_workflow()
-        print(f"Project {self.project_name} created successfully.")
+        with ProgressIndicator(message=f'Generating project {self.project_name}') as progress:
+            os.makedirs(f'workspace_{self.project_name}', exist_ok=True)
+            self._create_file('setup_py_template.py', 'setup.py')
+            self._create_file('setup_cfg_template.cfg', 'setup.cfg')
+            self._create_file('manifest_template.in', 'MANIFEST.in')
+            self._create_file('readme_template.md', 'README.md')
+            self._create_github_workflow()
+            self._create_folder_project()
+            progress.stop(final_message=f"Project {self.project_name} created successfully.")
     
     def _create_file(self, template_file, output_file):
-        with open(os.path.join(self.templates_dir, template_file)) as src_file:
-            content = src_file.read()
-            content = content.format(
-                project_name=self.project_name,
-                author=self.author,
-                email=self.email
-            )
-        with open(os.path.join(self.project_name, output_file), 'w') as dest_file:
-            dest_file.write(content)
+        with ProgressIndicator(message=f'Generating file {output_file}') as progress:
+            with open(os.path.join(self.templates_dir, template_file)) as src_file:
+                content = src_file.read()
+                content = content.format(
+                    project_name=self.project_name,
+                    author=self.author,
+                    email=self.email
+                )
+            with open(os.path.join(f'workspace_{self.project_name}', output_file), 'w') as dest_file:
+                dest_file.write(content)
+            progress.stop(final_message=f"created successfully {output_file}")
     
     def _create_github_workflow(self):
-        workflow_dir = os.path.join(self.project_name, '.github', 'workflows')
+        workflow_dir = os.path.join(f'workspace_{self.project_name}', '.github', 'workflows')
         os.makedirs(workflow_dir, exist_ok=True)
         self._create_file('publish_yml_template.yml', os.path.join('.github', 'workflows', 'publish.yml'))
+    def _create_folder_project(self):
+        workflow_dir = os.path.join(f'workspace_{self.project_name}', self.project_name)
+        os.makedirs(workflow_dir, exist_ok=True)
+        self._create_file('file_project_template.py', os.path.join(self.project_name,f'{self.project_name}.py'))
 
 def main():
     import argparse
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
+import setuptools_scm
 
 setup(
     name="pipsetup",
-    version="0.1.0",
+    use_scm_version=True,
+    setup_requires=["setuptools>=42", "setuptools_scm"],
     packages=find_packages(include=['pipsetup', 'pipsetup.*']),
     include_package_data=True,
     package_data={
